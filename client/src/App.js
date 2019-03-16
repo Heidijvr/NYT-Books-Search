3aab226c615259226069bb8538ca765d7d90d429
Heidijvr <43320863+Heidijvr@users.noreply.github.com>
2019-03-15 22:37:10 -0700

    Fixed form

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Saved from "./pages/Saved";
 import NoMatch from "./pages/NoMatch";
@@ -10,11 +10,11 @@ function App() {
     <Router>
       <div>
         <Nav />
-        <Switch routes={[
-        { path: "/", exact: true, component: Home },
-        { path: "/saved", exact: true, component: {Saved} },
-        { component: NoMatch }
-         ]} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/saved" component={Saved} />
+          <Route component={NoMatch} />
+        </Switch>
       </div>
     </Router>
   );
